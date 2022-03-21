cecdecdb75069e13c133178b560272f4ede1553c
stepaaahoris <stepan.jiri.horak@gmail.com>
2022-03-21 13:26:40 +0100

    Přidejte soubory projektu.

---
+++ Samostatná práce.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+
+int soucet(cislo) {
+	int suda = 0;
+	for (int i = 0; i <= cislo; i += 2) {
+		suda += i;
+	}
+	return suda;
+}
+
+
+int main()
+{
+	printf("suda %d\n", soucet(5));
+}
